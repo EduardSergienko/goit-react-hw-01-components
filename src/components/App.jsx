fa39869bebb61c00e7d7c436089018e598a1ee87
EduardSergienko <katleraster@gmail.com>
2022-06-26 19:22:40 +0300

    in process

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
+import { GlobalStyle } from './globalStyles';
+
+import Profile from './Profile/profile';
+import Statistic from './Statistics/statistics';
+import FriendList from './FriendList/friendList';
+import user from '../data/user.json';
+import data from '../data/data.json';
+import friends from 'data/friends.json';
+console.log(user);
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <GlobalStyle />
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistic title="Upload stats" stats={data} />
+      <Statistic stats={data} />
+      <FriendList friends={friends} />
+    </>
   );
 };
